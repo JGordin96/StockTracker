4cbefb6721e17d255006ac6359a15d402e548d6b
dedgv <dedgv@DESKTOP-OTE06QF>
2021-08-21 21:41:02 -0400

    Add project files.

---
+++ StockTracker/Models/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockTracker.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Ticker { get; set; }
+    }
+}
